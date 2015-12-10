b4a6332211ec968b4a611449a40a60606245b790
Matt Mills <mmills@bleacherreport.com>
2015-12-09 17:19:32 -0800

    WIP going home

--- lib/ecto_neo4j.ex
+++ lib/ecto_neo4j.ex
@@ -1,5 +1,5 @@
 defmodule Ecto.Neo4j do
-  def __before_compile__(_something) do
+  def __before_compile__(_x) do
 
   end
 
@@ -8,7 +8,13 @@ defmodule Ecto.Neo4j do
   end
 
   def storage_up(_something) do
-
+    :ok
   end
 
+  def start_link(repo, opts) do
+    {:ok, _} = Application.ensure_all_started(:ecto_neo4j)
+
+    repo.start_link(opts)
+    # Neo4j.Sips.start(:Asdf, :asdf)
+  end
 end
